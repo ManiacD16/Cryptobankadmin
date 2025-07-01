c7db701f9f35bc77e62cb3848f408a19d8b7ec8e
Manish Singh <msd161918@gmail.com>
2025-07-01 22:04:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./components/Login";
+// import QRCodeGenerator from "./components/QRCodeGenerator";
+import AdminPanel from "./components/AdminPanel";
+
+const App = () => {
+  return (
+    // <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        {/* <div className="container mx-auto mt-6"> */}
+          <Routes>
+            <Route path="/" element={<Login />} />
+            {/* <Route path="/payment" element={<QRCodeGenerator />} /> */}
+            <Route path="/admin" element={<AdminPanel />} />
+          </Routes>
+        {/* </div> */}
+      </div>
+    //  </Router>
+  );
+};
+
+export default App;
